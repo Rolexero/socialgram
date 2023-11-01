c882e0d15cf7f295504abcc1aa82d4b1eca23a02
Rolexero <rotimiolaolu@gmail.com>
2023-11-01 08:33:31 +0100

    style: :lipstick: complete the sign up form design

--- src/_auth/forms/Signinform.tsx
+++ src/_auth/forms/Signinform.tsx
@@ -1,18 +1,5 @@
-import { Button } from "@/components/ui/button";
-import * as z from "zod";
-
-// const formSchema = z.object({
-//   username: z.string().min(2, {
-//     message: "Username must be at least 2 characters.",
-//   }),
-// });
-
 const Signinform = () => {
-  return (
-    <div>
-      <Button>Button</Button>
-    </div>
-  );
+  return <div>Signupform</div>;
 };
 
 export default Signinform;
